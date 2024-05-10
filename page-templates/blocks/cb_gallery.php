28cfc49a6c5332a8de1c4e86781f0179de636d16
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-05-10 15:03:14 +0100

    Update cb_gallery.php

--- page-templates/blocks/cb_gallery.php
+++ page-templates/blocks/cb_gallery.php
@@ -1,5 +1,15 @@
 <section class="the_gallery">
     <div class="container-xl py-5">
-        GALLERY
+        <?php
+        if (get_field('gallery') ?? null) {
+            foreach (get_field('gallery') as $g) {
+                ?>
+        <a href="<?=wp_get_attachment_image_url($g, 'full')?>"
+            data-fancybox="gallery" aria-label="View image">
+            <?=wp_get_attachment_image($g, 'full')?></a>
+        <?php
+            }
+        }
+        ?>
     </div>
 </section>
