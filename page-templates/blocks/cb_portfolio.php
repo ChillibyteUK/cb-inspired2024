69a43cc8412ce5d1754ad03f39bd84a54f6e8e82
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-13 13:46:49 +0000

    Update cb_portfolio.php

--- page-templates/blocks/cb_portfolio.php
+++ page-templates/blocks/cb_portfolio.php
@@ -1,6 +1,6 @@
 <section class="portfolio">
     <div class="container-xl my-auto py-5">
-        <h2 class="mb-5">Portfolio</h2>
+        <h2 class="mb-5" data-aos="fade">Portfolio</h2>
         <div class="portfolio__swiper swiper-container mb-4">
             <div class="swiper-wrapper">
                 <?php
