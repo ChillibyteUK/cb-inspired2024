2cc46d401f1cac7cdde92990b57380ef05501a0b
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-14 12:21:29 +0000

    Update header.php

--- header.php
+++ header.php
@@ -133,9 +133,9 @@ session_start();
                 </a>
 
                 <!-- Burger Menu for Mobile -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </div>
 
                 <!-- Navbar Links -->
                 <div class="collapse navbar-collapse" id="navbarNav">
