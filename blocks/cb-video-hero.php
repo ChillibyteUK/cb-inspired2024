8288218c79c949a6747e3221012c3c152839a75d
ChillibyteDan <dan@chillibyte.co.uk>
2026-02-27 14:35:17 +0000

    video hero images

--- blocks/cb-video-hero.php
+++ blocks/cb-video-hero.php
@@ -21,7 +21,7 @@ $video_url = get_field( 'video' );
                     <img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/ied_logo_white.svg' ); ?>" class="video_hero__logo" alt="Inspired Earth Design" width="340" height="150">
                     <h1 class="has-white-color mt-4"><?= esc_html( get_field( 'title' ) ); ?></h1>
                     <div class="has-600-font-size"><?= wp_kses_post( get_field( 'content' ) ); ?></div>
-                    <div class="d-flex flex-wrap gap-3 mt-4">
+                    <div class="d-flex flex-wrap gap-3 my-4">
                         <a href="/portfolio/" class="btn btn-white">Portfolio</a>
                         <a href="/contact-us/" class="btn btn-white">Enquire</a>
                     </div>
